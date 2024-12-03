5c68b9583ad27fc052312044ce2b4c74d32310e1
forgottenduck324 <forgottenduck@proton.me>
2024-12-03 15:32:58 -0800

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Allan Hsu on 2024-12-03.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
